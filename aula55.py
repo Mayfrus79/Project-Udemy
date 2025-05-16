e028e2a3962104867dc5ee10eb91b01e77968eb8
maykonjonattan <mayfrusmj@gmail.com>
2025-05-16 14:55:08 -0300

    Fix two minors erros in the files aula55.py and aula61.py

--- aula55.py
+++ aula55.py
@@ -16,6 +16,6 @@ def my_open(file_path, mode):
 # Uses the context manager to open and manipulate the file
 with my_open('aula150.txt', 'w') as file:
     file.write('Line 1\n')  # Writes the first line
-    file.write('Line 2\n', 123)  # Error: 'write' accepts only 1 argument
+    file.write('Line 2\n')  # Error: 'write' accepts only 1 argument
     file.write('Line 3\n')  # Writes the third line
     print('WITH', file)  # Prints the file
--- aula61.py
+++ aula61.py
@@ -1,31 +1,28 @@
-
 from datetime import datetime
-from dateutil.relativedelta import relativedelta
-
+from dateutil.relativedelta import relativedelta  # type: ignore
 
 total_amount = 1_000_000
-loan_date = datetime(2025,3,14)
-Years = relativedelta(years=10)
-final_date = loan_date + Years
+loan_date = datetime(2025, 3, 14)
+loan_duration = relativedelta(years=10)
+final_date = loan_date + loan_duration
 
 installment_dates = []
 installment_date = loan_date
 
-while installment_date < final_date:
+while installment_date <= final_date:
     installment_dates.append(installment_date)
     installment_date += relativedelta(months=1)
 
-numbers_of_installments = (len(installment_dates))
-installment_value = total_amount / numbers_of_installments
+number_of_installments = len(installment_dates)
+installment_value = total_amount / number_of_installments
 
 for date in installment_dates:
     print(date.strftime('%Y/%m/%d'), f'${installment_value:,.2f}')
-    
+
 print()
 print(
-    f'You borrower ${total_amount:,.2f} to be paid',
-    f'in {Years.years} years',
-    f'in ({numbers_of_installments} months) in installments of '
+    f'You borrowed ${total_amount:,.2f} to be paid',
+    f'in {loan_duration.years} years',
+    f'({number_of_installments} months) in installments of '
     f'${installment_value:,.2f}.'
 )
-    
